750e10b68489bd0707e3a0e584b789fef362238e
cnlelema <lelema.cn@gmail.com>
2018-07-31 17:16:46 -0400

    debug

--- main.c
+++ main.c
@@ -10,16 +10,10 @@ int main(void){
     start_daytime();
     // start_chargen();
 
-
     sleep(2);
     printf("now start client...\n");
 
     run_client();
-
-    printf("server and client started\n");
-    for (i = 0; i < 10000; i++){
-        sleep(200000);
-    }
 
     return 0;
 }
